296f50e54b8b04234664dea11e716b21c24e5dca
GustavoMont <lgmluis@gmail.com>
2023-07-20 21:18:50 -0300

    solve mail module problem

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { join } from 'path';
+import { resolve } from 'path';
 import { emailConfig } from 'src/config/enviroment';
 
 @Module({
@@ -18,7 +18,7 @@ import { emailConfig } from 'src/config/enviroment';
         },
       },
       template: {
-        dir: join(__dirname, 'templates'),
+        dir: resolve(__dirname, 'templates'),
         adapter: new HandlebarsAdapter(),
         options: {
           strict: true,
